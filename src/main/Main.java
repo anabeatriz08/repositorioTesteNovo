6d8b0b40662849d80860a2f35e7e1aac8e441b4f
anabeatriz08 <aninha_luizrolim@hotmail.com>
2021-08-23 23:25:15 -0300

    commit de teste novo projeto

---
+++ src/main/Main.java
@@ -0,0 +1,21 @@
+package main;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double delta = 4 ;
+        double a = 1;
+        double b = 6;
+        double c = 2;
+
+        System.out.println(-b + Math.sqrt(Math.pow(delta,2) - 4 * a * c) / (2 * a));
+        System.out.println(-b - Math.sqrt(Math.pow(delta,2) - 4 * a * c) / (2 * a));
+
+
+
+
+    }
+
+
+}
